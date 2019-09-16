566089d07cf7cc6051a39305d6a058a3c17a3280
brunobannwart <bbannwart@yahoo.com.br>
2019-09-16 16:09:56 -0300

    Tratamento de Exceções

---
+++ src/compilator/LexicalException.java
@@ -0,0 +1,32 @@
+package compilator;
+
+public class LexicalException extends Exception{
+
+    public void digitError(){
+        // TODO
+    }
+    
+    public void letterError(){
+        // TODO
+    }
+    
+    public void attributionError(){
+        // TODO
+    }
+    
+    public void aritmeticError(){
+        // TODO
+    }
+    
+    public void relationalError(){
+        // TODO
+    }
+    
+    public void punctuationError(){
+        // TODO
+    }
+    
+    public void characterInvalid(){
+        // TODO
+    }
+}
